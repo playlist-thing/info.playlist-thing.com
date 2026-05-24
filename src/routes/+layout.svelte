dda6232643e24228408208afc36894d1c41dcc88
joachimschmidt557 <joachim.schmidt557@outlook.com>
2026-05-24 21:06:56 +0200

    add base style

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	let { children } = $props();
+</script>
+
+<div class="page">
+	<main class="content">
+		{@render children()}
+	</main>
+</div>
+
+<style>
+	.page {
+		display: flex;
+		justify-content: center;
+	}
+
+	.content {
+		max-width: 1000px;
+
+		padding-left: 5px;
+		padding-right: 5px;
+	}
+</style>
--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,1 +1,4 @@
+import '@fontsource-variable/work-sans';
+import '$lib/style/base.css';
+
 export const prerender = true;
